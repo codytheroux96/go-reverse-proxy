8641f089b2802eee2eb8b54b50599f470ee3eb58
codytheroux96 <cody.theroux3@gmail.com>
2025-03-05 19:22:11 -0500

    getting small server up for the main proxy server

--- cmd/go_reverse_proxy/main.go
+++ cmd/go_reverse_proxy/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"net/http"
+	"os"
+	"time"
+
 	"github.com/codytheroux96/go-reverse-proxy/internal/app"
 	"github.com/codytheroux96/go-reverse-proxy/test_servers/server_one"
 	"github.com/codytheroux96/go-reverse-proxy/test_servers/server_two"
@@ -10,6 +14,14 @@ func main() {
 	app := app.NewApplication()
 	app.Logger.Info("MESSAGE FROM MAIN SERVER: APPLICATION IS RUNNING!!!")
 
+	proxyServer := &http.Server{
+		Addr: ":8080",
+		// Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
 	go func() {
 		server_one.ServerOne()
 	}()
@@ -18,5 +30,10 @@ func main() {
 		server_two.ServerTwo()
 	}()
 
+	err := proxyServer.ListenAndServe()
+	if err != nil {
+		app.Logger.Error(err.Error())
+		os.Exit(1)
+	}
 	select {}
 }
