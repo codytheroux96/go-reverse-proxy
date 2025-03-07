42cfc6a3c5220f796c2c8457ff5720e754157fbe
codytheroux96 <cody.theroux3@gmail.com>
2025-03-07 12:14:59 -0500

    starting to handle post request - needs work

--- internal/app/handlers.go
+++ internal/app/handlers.go
@@ -24,8 +24,41 @@ func (app *Application) HandleServerOneGet (w http.ResponseWriter, r *http.Reque
 	w.Write(body)
 }
 
+func (app *Application) HandleServerOnePost (w http.ResponseWriter, r *http.Request) {
+	resp, err := http.Get("http://localhost:4200/s1list")
+	if err != nil {
+		fmt.Println("not hitting server one")
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("could not read body on server one")
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(body)
+}
+
 func (app *Application) HandleServerTwoGet (w http.ResponseWriter, r *http.Request) {
 	resp, err := http.Get("http://localhost:2200/s2health")
+	if err != nil {
+		fmt.Println("not hitting server two")
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("could not read body on server two")
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(body)
+}
+
+func (app *Application) HandleServerTwoPost (w http.ResponseWriter, r *http.Request) {
+	resp, err := http.Get("http://localhost:2200/s2list")
 	if err != nil {
 		fmt.Println("not hitting server two")
 	}
--- internal/app/routes.go
+++ internal/app/routes.go
@@ -10,7 +10,10 @@ func (app *Application) Routes() http.Handler {
 	r := chi.NewRouter()
 
 	r.Get("/s1get", app.HandleServerOneGet)
-	r.Get("/s2get", app.HandleServerTwoGet)
+	r.Get("/s2get", app.HandleServerTwoGet) 
+
+	r.Post("/s1post", app.HandleServerOnePost)
+	r.Post("/s1post", app.HandleServerTwoPost)
 
 	return r
 }
