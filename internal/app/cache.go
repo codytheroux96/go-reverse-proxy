fd7fd2f831d75992281b6b003f8f1fd3403ecabb
codytheroux96 <cody.theroux3@gmail.com>
2025-03-14 21:57:11 -0400

    made cache struct and cache entry struct

--- internal/app/cache.go
+++ internal/app/cache.go
@@ -1,4 +1,17 @@
 package app
 
+import (
+	"sync"
+	"time"
+)
+
 type ResponseCache struct {
+	mu    sync.RWMutex
+	cache map[string]CacheEntry
+	ttl   time.Duration
+}
+
+type CacheEntry struct {
+	response []byte
+	expires  time.Time
 }
