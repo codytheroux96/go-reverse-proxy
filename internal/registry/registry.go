801ce4bb81cb47ef603be0bfd13f6f9b587e93b6
codytheroux96 <cody.theroux3@gmail.com>
2025-03-28 17:33:07 -0400

    made initial registry methods

--- internal/registry/registry.go
+++ internal/registry/registry.go
@@ -1,6 +1,7 @@
 package registry
 
 import (
+	"fmt"
 	"sync"
 	"time"
 )
@@ -21,4 +22,40 @@ func (r *Registry) NewRegistry() *Registry {
 	return &Registry{
 		servers: make(map[string]Server),
 	}
+}
+
+func (r *Registry) Register(s Server) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	if _, exists := r.servers[s.Name]; exists {
+		return fmt.Errorf("server '%s' already registered", s.Name)
+	}
+
+	r.servers[s.Name] = s
+	return nil
+}
+
+func (r *Registry) Deregister(name string) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	if _, exists := r.servers[name]; !exists {
+		return fmt.Errorf("server '%s' does not exist... cannot deregister", name)
+	}
+
+	delete(r.servers, name)
+	return nil
+} 
+
+func (r *Registry) ListRegistered() []Server {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
+	servers := make([]Server, 0, len(r.servers))
+	for _, server := range r.servers {
+		servers = append(servers, server)
+	}
+
+	return servers
 }
